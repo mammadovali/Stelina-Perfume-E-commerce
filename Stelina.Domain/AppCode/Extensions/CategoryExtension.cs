9042eaceba06fe03e490e12b50ed010f87bf308c
mammadovali <aliam@code.edu.az>
2023-01-20 02:05:55 +0400

    CategoriesController

---
+++ Stelina.Domain/AppCode/Extensions/CategoryExtension.cs
@@ -0,0 +1,31 @@
+﻿using Stelina.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stelina.Domain.AppCode.Extensions
+{
+    static public partial class CategoryExtension
+    {
+        static public IEnumerable<Category> GetAllChildren(this Category category)
+        {
+            if (category.ParentId != null)
+            {
+                yield return category;
+            }
+
+
+            if (category.Children != null)
+            {
+                foreach (var item in category.Children.SelectMany(c => c.GetAllChildren()))
+                {
+                    yield return item;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Stelina.WebUI/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Stelina.Domain.Models.DataContexts;
+using Stelina.Domain.Models.Entities;
+
+namespace Stelina.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+        private readonly StelinaDbContext _context;
+
+        public CategoriesController(StelinaDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Policy = "admin.categories.index")]
+        public async Task<IActionResult> Index()
+        {
+            var stelinaDbContext = _context.Categories;
+            return View(await stelinaDbContext.ToListAsync());
+        }
+
+        [Authorize(Policy = "admin.categories.details")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories
+                .Include(c => c.Parent)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [Authorize(Policy = "admin.categories.create")]
+        public IActionResult Create()
+        {
+            ViewData["ParentId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "admin.categories.create")]
+        public async Task<IActionResult> Create([Bind("ParentId,Name,Id,CreatedDate,DeletedDate")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(category);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentId"] = new SelectList(_context.Categories, "Id", "Name", category.ParentId);
+            return View(category);
+        }
+
+        [Authorize(Policy = "admin.categories.edit")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            ViewData["ParentId"] = new SelectList(_context.Categories, "Id", "Name", category.ParentId);
+            return View(category);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "admin.categories.edit")]
+        public async Task<IActionResult> Edit(int id, [Bind("ParentId,Name,Id,CreatedDate,DeletedDate")] Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryExists(category.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ParentId"] = new SelectList(_context.Categories, "Id", "Name", category.ParentId);
+            return View(category);
+        }
+
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "admin.categories.delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _context.Categories.Any(e => e.Id == id);
+        }
+    }
+}
